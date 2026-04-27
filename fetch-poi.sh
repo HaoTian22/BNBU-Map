12109358c70f91fcafb72034041a4c678522f815
Hao_Tian22 <hao_tian22@outlook.com>
2026-04-27 14:35:00 +0800

    overpass

--- fetch-poi.sh
+++ fetch-poi.sh
@@ -4,7 +4,7 @@
 cd "$(dirname "$0")" || exit 1
 
 # Overpass API 端点
-OVERPASS_API="https://overpass.private.coffee/api/interpreter"
+OVERPASS_API="https://overpass-api.de/api/interpreter"
 
 # 输出文件
 OUTPUT_FILE="POI.json"
